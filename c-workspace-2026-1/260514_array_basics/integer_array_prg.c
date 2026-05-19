f7dbd8761d0487181c9c32c38eec73d46e6a52a1
CHENGJUHO <chengjuho@naver.com>
2026-05-19 20:06:29 +0900

    feat: 배열 기초 실습 (정수 배열·최대/최소 탐색)

---
+++ c-workspace-2026-1/260514_array_basics/integer_array_prg.c
@@ -0,0 +1,53 @@
+﻿//0~9 사이의 정수 10개를 입력받아 배열에 저장한다.
+// 0~9 사이의 수가 아니면 오류 출력 후 재입력
+// 중복 입력된 수(들) 출력
+// 미입력된 수(들) 출력
+// 6 1 2 3 4 2 5 7 8 3
+#define _CRT_SECURE_NO_WARNINGS
+#define ARRSIZE 5
+#define CHECK 10
+#define TRUE 1
+#include <stdio.h>
+
+int main() 
+{
+	int iArrays[ARRSIZE];
+	int iCount[CHECK] = { 0 };
+	int iIndex, iDuplicate, iMiss;
+	printf("정수 10개를 입력: ");
+	for (int i = 0; i < ARRSIZE; ++i) {
+		scanf("%d", &iArrays[i]);
+		//중복 입력된 수 체크
+		iIndex = iArrays[i];
+		iCount[iIndex]++;
+		if (iArrays[i] < 0 || iArrays[i] > 9) { // 0~9 사이의 수가 아니면 오류 출력 후 재입력
+			printf("[ERROR] 0~9사이 정수아님. 다시 입력\n");
+			--i;
+		}
+	}
+	printf("입력된 수:");
+	for(int t = 0; t < ARRSIZE; ++t) {
+		printf("%d ", iArrays[t]);
+	}
+	
+	printf("\n중복입력된 수: ");
+	for (int j = 0; j < CHECK; ++j) {
+		if (iCount[j] >= 2) { // 중복 입력된 수 출력
+			iDuplicate = j;
+			printf("%d ", iDuplicate);
+		}
+	}
+	printf("\n카운트 배열 확인 : ");
+	for (int f = 0; f < CHECK; ++f) {
+		printf("%d ", iCount[f]);
+	}
+	printf("\n미입력된 수: ");
+	for (int k = 0; k < CHECK; ++k) {
+		if (iCount[k] == 0) { // 미입력된 수(들) 출력
+			iMiss = k;
+			printf("%d ", iMiss);
+		}
+	}
+
+	return 0;
+}
---
+++ c-workspace-2026-1/260519_array_min_max/arr_min_max_prg.c
@@ -0,0 +1,84 @@
+﻿// 정수 10개를 입력받아 최솟값과 최댓값을 찾는 프로그램을 작성하시오
+// 정수 10개를 입력하여 배열에 저장한다
+// 입력된 수 중에서 최솟값과 최댓값을 출력 한다
+// 두번째 작은 수 찾기
+#define _CRT_SECURE_NO_WARNINGS
+#define ARRSIZE 10
+#include <stdio.h>
+int getMinValue(int iArrays[]);
+int getMaxValue(int iArrays[]);
+void sortArr(int iArrays[]);
+void printArr(int iArrays[]);
+//4 6 3 8 2 5 3 7 9 1
+int main() {
+	int iArrays[ARRSIZE] = {4, 6, 3, 8, 2, 5, 3, 7, 9, 1};
+	int iMin, iMax;
+	/*for (int iInputIndex = 0; iInputIndex < ARRSIZE; ++iInputIndex) {
+		printf("%d번째 정수 입력 : ", iInputIndex + 1);
+		scanf("%d", &iArrays[iInputIndex]);
+	}*/
+	iMin = getMinValue(iArrays);
+	iMax = getMaxValue(iArrays);
+	printArr(iArrays);
+	printf("최댓값은 : %d\n", iMax);
+	printf("최솟값은 : %d\n", iMin);
+	sortArr(iArrays);
+	printArr(iArrays);
+	//두번째 최솟값
+	printf("두번째 최솟값 : %d", iArrays[1]);
+
+	return 0;
+}
+//1부터 탐색 ,,  각 과정을 분리 >> 각자의 함수로
+int getMinValue(int iArrays[]) {
+	int iMin;
+	iMin = iArrays[0];
+	for (int iMinSearch = 1; iMinSearch < ARRSIZE; ++iMinSearch) {
+		if (iArrays[iMinSearch] < iMin) {
+			iMin = iArrays[iMinSearch];
+		}
+	}
+	return iMin;
+}
+
+int getMaxValue(int iArrays[]) {
+	int iMax;
+	iMax = iArrays[0];
+	for (int iMaxSearch = 1; iMaxSearch < ARRSIZE; ++iMaxSearch) {
+		if (iArrays[iMaxSearch] > iMax) {
+			iMax = iArrays[iMaxSearch];
+		}
+	}
+	return iMax;
+}
+
+void sortArr(int iArrays[]) 
+{
+	int iMin, iIndex, iTemp[ARRSIZE];
+
+	for (int iLoop = 0; iLoop < ARRSIZE; ++iLoop)
+	{
+		iMin = iArrays[iLoop];
+		iIndex = iLoop;
+		
+		for (int j = iLoop; j < ARRSIZE; ++j)
+		{
+			if (iMin > iArrays[j])
+			{
+				iMin = iArrays[j];
+				iIndex = j;
+			}
+		}
+		iTemp[iLoop] = iArrays[iLoop];
+		iArrays[iLoop] = iArrays[iIndex];
+		iArrays[iIndex] = iTemp[iLoop];
+	}
+}
+void printArr(int iArrays[]) {
+	//배열 출력
+	printf("배열 원소 : { ");
+	for (int iPrintIndex = 0; iPrintIndex < ARRSIZE; ++iPrintIndex) {
+		printf("%d ", iArrays[iPrintIndex]);
+	}
+	printf("}\n");
+}
